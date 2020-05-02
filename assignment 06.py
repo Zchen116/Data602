a1c651676ad5e775738dde2695a7cd79a12e279a
Zchen116 <46012842+Zchen116@users.noreply.github.com>
2020-05-02 16:46:00 -0400

    Add files via upload

---
+++ assignment 06.py
@@ -0,0 +1,52 @@
+from flask import Flask, request
+import re
+
+main_page = '''
+<html>
+    <head>
+    <title></title>
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css">
+    </head>
+<body>
+<form class="form-horizontal" method="post" action="/calc">
+<fieldset>
+<!-- Form Name -->
+<legend>Multiplier</legend>
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="textinput">Number</label>  
+  <div class="col-md-4">
+  <input id="textinput" type="number" name='number' placeholder="Enter a number" class="form-control input-md">
+  </div>
+</div>
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="singlebutton"></label>
+  <div class="col-md-4">
+    <button id="singlebutton" name="singlebutton" class="btn btn-primary">Calculate</button>
+  </div>
+</div>
+</fieldset>
+</form>
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+</body>
+</html>
+'''
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+   return render_template_string(main_page)
+
+
+@app.route('/calc', methods = ['POST'])
+def handle_data():
+    number = request.form['number']
+    return str(float(number)*5)
+  
+
+
+app.run()
